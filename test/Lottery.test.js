c2f550fbc3de37eaf7e818a76752fea17cdafb65
shrys <shreyas770@yahoo.in>
2018-09-13 19:37:59 +0530

    test entry for multiple players

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -11,7 +11,7 @@ let accounts;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    
+
     // use an account for deployment
     lottery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({
@@ -28,10 +28,38 @@ describe('Lottery Contract', () => {
     it('deploys contract', () => {
         assert.ok(lottery.options.address);
     });
+
     it('allows one account to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
             value: web3.utils.toWei('0.02', 'ether')
         });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+    });
+
+    it('allows multiple accounts', async () => {
+        let N = 3;
+        for (let account of accounts) {
+            await lottery.methods.enter().send({
+                from: account,
+                value: web3.utils.toWei('0.02', 'ether')
+            });
+        }
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        for (let a in accounts) {
+            assert(accounts[a], players[a]);
+        }
+
+        assert(accounts.length, players.length);
     });
 });
